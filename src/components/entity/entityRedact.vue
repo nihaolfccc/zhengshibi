bc5544a1ddc254ee7900fc6e180c44ab4dcee332
lifengcheng <lifengcheng@jusfoun.com>
2020-01-08 16:44:23 +0800

    bi版本2

---
+++ src/components/entity/entityRedact.vue
@@ -0,0 +1,45 @@
+<template>
+		<div class="entity_redact">
+			<div class="">
+				<img src="../../assets/imgs/red/entityName.png" alt="" />
+			</div>
+			<div>
+				<foreign-key-left-list-my></foreign-key-left-list-my>
+			</div>
+		</div>
+</template>
+
+<script>
+	import foreignKeyLeftListMy from "components/dataForeignKeyLeftList/foreignKeyLeftListMy"
+	export default {
+		components: {
+			foreignKeyLeftListMy
+		},
+		data() {
+			return {
+				
+			}
+		},
+		computed: {
+
+		},
+		watch: {
+
+		},
+		methods: {
+
+		},
+		created() {
+
+		},
+		mounted() {
+
+		}
+	}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+	.entity_redact{
+		
+	}
+</style>
